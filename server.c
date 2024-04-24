5e0c0c5ae3881f058682fc9997ae6b9115f31f20
Herinambinintsoa Nantenaina Ramarosandratana <hramaros@c1r10s2.42antananarivo.mg>
2024-04-24 07:11:18 +0300

    server - remove forbidden function

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: hramaros <hramaros@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 09:51:06 by hramaros          #+#    #+#             */
-/*   Updated: 2024/04/24 07:06:28 by hramaros         ###   ########.fr       */
+/*   Updated: 2024/04/24 07:10:51 by hramaros         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,8 @@
 int	main(void)
 {
 	int size;
-	int pipe;
-	int fd;
 
-	pipe = mkfifo(".fifo", 0777);
-	if (pipe == -1)
-	{
-		return (1);
-	}
-	fd = open(".fifo", O_RDONLY);
-	if (fd == -1)
-	{
-		return (2);
-	}
 	ft_printf("Server started... PID: %d\n\n", getpid());
-	read(fd, &size, sizeof(int) * 1);
 
 	return (0);
 }
