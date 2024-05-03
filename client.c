f7c9442882964f69008818446a5b3fbe482a09a8
Herinambinintsoa Nantenaina Ramarosandratana <hramaros@c1r10s2.42antananarivo.mg>
2024-05-03 06:38:41 +0300

    server.c - progress

--- client.c
+++ client.c
@@ -6,26 +6,26 @@
 /*   By: hramaros <hramaros@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 09:51:13 by hramaros          #+#    #+#             */
-/*   Updated: 2024/04/24 07:44:31 by hramaros         ###   ########.fr       */
+/*   Updated: 2024/04/27 09:35:19 by hramaros         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <signal.h>
 #include <sys/types.h>
 #include <unistd.h>
 
-int	ft_sendsize(const unsigned int serverPid, const unsigned int size)
-{
-	if (write(0, &size, sizeof(unsigned int)))
-		if (kill(serverPid, SIGUSR1) == 0)
-			return (1);
-	return (0);
-}
-
 int	ft_sendmessage(const unsigned int server_pid, const char *message)
 {
-	if (write(0, message, ft_strlen(message)))
+	if (ft_strlen(message) > 3)
+	{
+		if (kill(server_pid, SIGUSR1) == 0)
+			return (1);
+	}
+	else
+	{
 		if (kill(server_pid, SIGUSR2) == 0)
 			return (1);
+	}
 	return (0);
 }
 
@@ -39,16 +39,15 @@ int	main(int argc, char **argv)
 		write(1, "\nUtilisation: ./client [PID du serveur] [chaine]\n", 49);
 		write(1,
 			"\nExemple: ./client 4242 'bonjour c'est le projet Minitalk'\n\n",
-			60) exit(0);
+			60);
+		return (1);
 	}
 	server_pid = ft_atoi(argv[1]);
-	message_len = ft_strlen(argv[2]);
-	if (!ft_sendsize(server_pid, message_len) || !ft_sendmessage(server_pid,
-			argv[2]))
+	if (!ft_sendmessage(server_pid, argv[2]))
 	{
 		ft_printf("\nMessage non envoye au serveur %d, quitte le programme.",
 			server_pid);
-		exit(1);
+		return (1);
 	}
 	ft_printf("PID: %d\nMessage: '%s'\n", getpid(), argv[2]);
 	return (0);
--- server.c
+++ server.c
@@ -6,19 +6,81 @@
 /*   By: hramaros <hramaros@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 09:51:06 by hramaros          #+#    #+#             */
-/*   Updated: 2024/04/24 07:44:58 by hramaros         ###   ########.fr       */
+/*   Updated: 2024/04/27 10:47:06 by hramaros         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <fcntl.h>
+#include <signal.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
 
+#define MAX_CHAR 2048
+
+typedef struct s_data
+{
+	char			*buffer;
+	char			c;
+	unsigned int	bit_index;
+}					t_data;
+
+void	ft_add_bit_one(void)
+{
+	void bit_mask;
+
+	bit_mask = 0b10000000;
+	
+}
+
+void	ft_add_bit_one(void)
+{
+}
+
+int	ft_init_data(t_data *data)
+{
+	data->buffer = malloc(sizeof(unsigned char) * MAX_CHAR);
+	if (!data->buffer)
+		return (0);
+	data->c = 0;
+	data->bit_index = 0;
+	return (1);
+}
+
+void	sig_handler(int sig)
+{
+	if (sig == SIGUSR1)
+		ft_add_bit_one();
+	else if (sig == SIGUSR2)
+		ft_add_bit_zero();
+	return ;
+}
+
+t_data				data;
+
 int	main(void)
 {
-	int	size;
+	struct sigaction	mngt;
+	sigset_t			mask;
 
+	sigemptyset(&mask);
+	sigaddset(&mask, SIGUSR1);
+	sigaddset(&mask, SIGUSR2);
+	mngt.sa_mask = mask;
+	mngt.sa_handler = sig_handler;
+	if (!ft_init_data(data))
+	{
+		write(1, "There was an error for global variable allocation ...\n", 53);
+		return (1);
+	}
+	sigaction(SIGUSR1, &mngt, NULL);
+	sigaction(SIGUSR2, &mngt, NULL);
 	ft_printf("Server started... PID: %d\n\n", getpid());
+	while (1)
+	{
+		
+		nsleep(100);
+		printf("%s\n", data.buffer);
+	}
 	return (0);
 }
