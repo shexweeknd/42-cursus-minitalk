863defb3f9ed5d62810e117edebf70fad8e5ad1c
Herinambinintsoa Nantenaina Ramarosandratana <hramaros@c1r10s2.42antananarivo.mg>
2024-05-03 06:57:54 +0300

    client.c - seems ok

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: hramaros <hramaros@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/21 09:51:13 by hramaros          #+#    #+#             */
-/*   Updated: 2024/04/27 09:35:19 by hramaros         ###   ########.fr       */
+/*   Updated: 2024/05/03 06:55:54 by hramaros         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,49 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+int	ft_send_ending(const unsigned int server_pid)
+{
+	int	i;
+
+	i = 8;
+	while (i--)
+	{
+		if (kill(server_pid, SIGUSR2) == -1)
+			return (0);
+	}
+	return (1);
+}
+
+int	ft_send_char(const unsigned int server_pid, unsigned char c)
+{
+	unsigned char	bit;
+
+	bit = 0b10000000;
+	while (bit)
+	{
+		if (bit & c)
+		{
+			// envoi de signaux 1
+			if (kill(server_pid, SIGUSR1) == -1)
+				return (0);
+		}
+		else
+		{
+			// envoi de signaux 0
+			if (kill(server_pid, SIGUSR2) == -1)
+				return (0);
+		}
+		bit >>= 1;
+		usleep(100);
+	}
+	ft_send_ending(server_pid);
+	return (1);
+}
+
 int	ft_sendmessage(const unsigned int server_pid, const char *message)
 {
-	if (ft_strlen(message) > 3)
-	{
-		if (kill(server_pid, SIGUSR1) == 0)
-			return (1);
-	}
-	else
-	{
-		if (kill(server_pid, SIGUSR2) == 0)
-			return (1);
-	}
+	while (*message)
+		ft_send_char(server_pid, *message++);
 	return (0);
 }
 
